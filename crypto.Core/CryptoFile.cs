e24f21a415176f86dd136143aa74f2347d502271
slimetsp <theslimeplay1@gmail.com>
2020-03-12 17:31:01 +0100

    removed outdated doc from cryptofile

--- crypto.Core/CryptoFile.cs
+++ crypto.Core/CryptoFile.cs
@@ -9,18 +9,12 @@ namespace crypto.Core
         private FileInfo _fi;
         public FileInfo FileInfo
         {
-            get => _fi ?? (_fi = new FileInfo(FileName));
+            get => _fi ??= new FileInfo(FileName);
             set => _fi = value;
         }
 
         public bool Exists => FileInfo.Exists;
 
-        /// <summary>
-        /// Constructs a new <see cref="CryptoFile"/>
-        /// </summary>
-        /// <param name="fileName">Path of the file</param>
-        /// <param name="fileInfo">Optional FileInfo</param>
-        /// <exception cref="IOException">Thrown when the file does not exists</exception>
         public CryptoFile(string fileName, FileInfo fileInfo = null)
         {
             FileName = fileName;
