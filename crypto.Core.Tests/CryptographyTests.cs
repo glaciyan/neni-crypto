8db2bf0c8e77be201a93ecc5226be5dfc82dccb8
slimetsp <theslimeplay1@gmail.com>
2020-03-25 22:28:34 +0100

    disabled resharper typos for mock strings

--- crypto.Core.Tests/CryptographyTests.cs
+++ crypto.Core.Tests/CryptographyTests.cs
@@ -11,6 +11,7 @@ namespace crypto.Core.Tests
         [Test]
         public void Data_Gets_Encrypted_Decrypted_Correctly_Long_String()
         {
+            // ReSharper disable StringLiteralTypo
             const string data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in mauris justo. In ut lacinia enim. Phasellus eu porta nunc. Mauris finibus dui at nulla mattis, vel consequat odio posuere. Proin pretium urna et orci vulputate, ac rutrum urna iaculis. Vestibulum sodales lobortis mollis. Sed fringilla mauris sed nisi imperdiet iaculis.";
             Data_Gets_Encrypted_Decrypted_Correctly(data);
         }
