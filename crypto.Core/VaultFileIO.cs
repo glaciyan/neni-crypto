07a0a933c167abe5dca61a68130d07c3d94d5661
slimetsp <theslimeplay1@gmail.com>
2020-03-26 11:30:17 +0100

    vaultfileio is a better name

---
+++ crypto.Core/VaultFileIO.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Text;
+using crypto.Core.Cryptography;
+
+namespace crypto.Core
+{
+    public static class VaultFileFormat
+    {
+        public static void WriteTo(Stream destination, VaultFile vaultFile)
+        {
+        }
+    }
+}
