3c2a30e9cd57eb507cfe4017e1424cb44851515e
slimetsp <theslimeplay1@gmail.com>
2020-04-03 09:48:48 +0200

    Formatted AesBytes.cs

--- crypto.Core/Cryptography/AesBytes.cs
+++ crypto.Core/Cryptography/AesBytes.cs
@@ -12,7 +12,7 @@ namespace crypto.Core.Cryptography
         public AesBytes(KeyIVPair keyIvPair)
         {
             _keyIvPair = keyIvPair;
-            
+
             // create aes with preferred settings
             _aes = Aes.Create();
             _aes.KeySize = 256;
@@ -25,10 +25,15 @@ namespace crypto.Core.Cryptography
         {
         }
 
+        public void Dispose()
+        {
+            _aes?.Dispose();
+        }
+
         public byte[] EncryptBytes(byte[] plainText)
         {
             // get the size with spacing for padding
-            var outputSize = plainText.Length + (AesBlockSize - (plainText.Length % AesBlockSize));
+            var outputSize = plainText.Length + (AesBlockSize - plainText.Length % AesBlockSize);
             var outputBuffer = new byte[outputSize];
 
             using var encryptTransform = _aes.CreateEncryptor();
@@ -36,14 +41,13 @@ namespace crypto.Core.Cryptography
             // counts the transformed bytes
             var transformed = 0;
             while (transformed < plainText.Length - AesBlockSize)
-            {
                 transformed += encryptTransform.TransformBlock(
                     plainText, transformed, AesBlockSize,
                     outputBuffer, transformed);
-            }
 
             // last block adds padding which is required for decryption
-            var lastBytes = encryptTransform.TransformFinalBlock(plainText, transformed, plainText.Length - transformed);
+            var lastBytes =
+                encryptTransform.TransformFinalBlock(plainText, transformed, plainText.Length - transformed);
             outputBuffer.SetRange(transformed, lastBytes, 0, AesBlockSize);
 
             return outputBuffer;
@@ -52,11 +56,12 @@ namespace crypto.Core.Cryptography
         public byte[] DecryptBytes(byte[] cipherText)
         {
             // create a buffer, the last two blocks are in the final buffer if the data is big enough
-            var buffer = cipherText.Length <= AesBlockSize ?
-                new byte[AesBlockSize] : new byte[cipherText.Length - AesBlockSize * 2];
+            var buffer = cipherText.Length <= AesBlockSize
+                ? new byte[AesBlockSize]
+                : new byte[cipherText.Length - AesBlockSize * 2];
 
             using var decryptTransform = _aes.CreateDecryptor();
-            
+
             var transformed = 0;
             while (transformed < cipherText.Length - AesBlockSize)
             {
@@ -65,9 +70,9 @@ namespace crypto.Core.Cryptography
                 // decrypt transform decrypts the saved block in the second run that's why
                 // outputOffset is reduced by the aes block size to include the last block
                 decryptTransform.TransformBlock(
-                    cipherText, transformed, AesBlockSize, 
+                    cipherText, transformed, AesBlockSize,
                     buffer, transformed - AesBlockSize);
-                
+
                 transformed += AesBlockSize;
             }
 
@@ -76,17 +81,12 @@ namespace crypto.Core.Cryptography
 
             // you only need to return the final block when the encrypted data smaller is than the block size
             if (finalBlock.Length < AesBlockSize) return finalBlock;
-            
+
             // create array with space for both buffer and last bytes
             var output = new byte[buffer.Length + finalBlock.Length];
             output.CombineFrom(buffer, finalBlock);
-            
-            return output;
-        }
 
-        public void Dispose()
-        {
-            _aes?.Dispose();
+            return output;
         }
     }
 }
