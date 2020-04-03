af70fdf9f0f9327aeb30336369eb82a75bac6bc1
slimetsp <theslimeplay1@gmail.com>
2020-04-03 09:59:58 +0200

    Commented out class for stream copying which creates hashes

---
+++ crypto.Core/Cryptography/VerifyingStream.cs
@@ -0,0 +1,18 @@
+// using System.IO;
+//
+// namespace crypto.Core.Cryptography
+// {
+//     public static class VerifyingStream
+//     {
+//         public static byte[] CopyToCreateHash(this Stream source, Stream destination)
+//         {
+//             
+//         }
+//         
+//         public static bool CopyToVerifyHash(this Stream source, Stream destination, byte[] verificationHash)
+//         {
+//             
+//         }
+//     }
+// }
+
