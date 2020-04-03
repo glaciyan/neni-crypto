707026cb22de3553c29346ae06706813b34ec44e
slimetsp <theslimeplay1@gmail.com>
2020-04-03 20:17:01 +0200

    Moved reading to another class

---
+++ crypto.Core/File/VaultFileReader.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using System.Text;
+using crypto.Core.Cryptography;
+
+namespace crypto.Core.File
+{
+    public class VaultFileReader
+    {
+        public static VaultFile ReadFrom(Stream source)
+        {
+            using var binReader = new BinaryReader(source, Encoding.Unicode, true);
+            var result = new VaultFile();
+            
+            // info from plaintext
+            var plainIV = binReader.ReadBytes(AesSizes.IV);
+            var plainNameLength = binReader.ReadInt32();
+            var encryptedPlainName = binReader.ReadBytes(plainNameLength);
+            
+            result.SecuredPlainName = new SecretFileName(encryptedPlainName, plainIV);
+
+            result.TargetCipherIV = binReader.ReadBytes(AesSizes.IV);
+            result.TargetAuthentication = binReader.ReadBytes(AesSizes.Auth);
+            result.TargetPath = binReader.ReadString();
+            
+            var isUnlocked = binReader.ReadBoolean();
+
+            if (isUnlocked)
+            {
+                var unlockedFilePathIV = binReader.ReadBytes(AesSizes.IV);
+                var length = binReader.ReadInt32();
+                var secretUnlockedFilePath = binReader.ReadBytes(length);
+                
+                result.UnlockedFilePath = new SecretFileName(secretUnlockedFilePath, unlockedFilePathIV);
+            }
+
+            return result;
+        }
+    }
+}
--- crypto.Core/File/VaultFileWriter.cs
+++ crypto.Core/File/VaultFileWriter.cs
@@ -12,12 +12,12 @@ namespace crypto.Core.File
         Writer
     }
     
-    public class VaultFileReaderWriter
+    public class VaultFileWriter
     {
         private readonly VaultFile _underlying;
         private readonly byte[] _key;
         
-        public VaultFileReaderWriter(VaultFile underlying, byte[] key)
+        public VaultFileWriter(VaultFile underlying, byte[] key)
         {
             _underlying = underlying;
             _key = key;
@@ -49,36 +49,6 @@ namespace crypto.Core.File
                 binWriter.Write(secretUnlockedFilePath.Length);
                 binWriter.Write(secretUnlockedFilePath);
             }
-        }
-
-        public static VaultFile ReadFrom(Stream source)
-        {
-            using var binReader = new BinaryReader(source, Encoding.Unicode, true);
-            var result = new VaultFile();
-            
-            // info from plaintext
-            var plainIV = binReader.ReadBytes(AesSizes.IV);
-            var plainNameLength = binReader.ReadInt32();
-            var encryptedPlainName = binReader.ReadBytes(plainNameLength);
-            
-            result.SecuredPlainName = new SecretFileName(encryptedPlainName, plainIV);
-
-            result.TargetCipherIV = binReader.ReadBytes(AesSizes.IV);
-            result.TargetAuthentication = binReader.ReadBytes(AesSizes.Auth);
-            result.TargetPath = binReader.ReadString();
-            
-            var isUnlocked = binReader.ReadBoolean();
-
-            if (isUnlocked)
-            {
-                var unlockedFilePathIV = binReader.ReadBytes(AesSizes.IV);
-                var length = binReader.ReadInt32();
-                var secretUnlockedFilePath = binReader.ReadBytes(length);
-                
-                result.UnlockedFilePath = new SecretFileName(secretUnlockedFilePath, unlockedFilePathIV);
-            }
-
-            return result;
         }
     }
 }
