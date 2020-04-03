1459acca2e21e4ae076e8ec05aa488c10c6cd1a3
slimetsp <theslimeplay1@gmail.com>
2020-04-03 09:53:12 +0200

    Clean up from Rider

--- crypto.Core/ArrayExtension.cs
+++ crypto.Core/ArrayExtension.cs
@@ -6,24 +6,18 @@ namespace crypto.Core
     {
         public static void SetRange<T>(this T[] src, int srcOffset, T[] dest, int destOffset, int range)
         {
-            if (src.Length - srcOffset - dest.Length - range == 0) throw new ArgumentOutOfRangeException(nameof(dest), "The range doesn't fit in the array");
+            if (src.Length - srcOffset - dest.Length - range == 0)
+                throw new ArgumentOutOfRangeException(nameof(dest), "The range doesn't fit in the array");
 
-            for (var i = 0; i < range; i++)
-            {
-                src[srcOffset + i] = dest[destOffset + i];
-            }
+            for (var i = 0; i < range; i++) src[srcOffset + i] = dest[destOffset + i];
         }
 
         public static void CombineFrom<T>(this T[] dest, params T[][] sources)
         {
             var i = 0;
             foreach (var source in sources)
-            {
-                foreach (var v in source)
-                {
-                    dest[i++] = v;
-                }
-            }
+            foreach (var v in source)
+                dest[i++] = v;
         }
     }
 }
