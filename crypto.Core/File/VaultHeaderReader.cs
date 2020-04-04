766a1b2e6322d89d765ef3758a47bbb6734106aa
slimetsp <theslimeplay1@gmail.com>
2020-04-04 09:44:07 +0200

    Implemented reader

---
+++ crypto.Core/File/VaultHeaderReader.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Text;
+using crypto.Core.Cryptography;
+
+namespace crypto.Core.File
+{
+    public static class VaultHeaderReader
+    {
+        public static VaultHeader ReadFrom(Stream source, byte[] key)
+        {
+            using var binReader = new BinaryReader(source, Encoding.Unicode, true);
+
+            var magicNumber = binReader.ReadBytes(VaultHeader.MagicNumberLength);
+
+            if (!magicNumber.ContentEqualTo(VaultHeader.MagicNumber))
+                throw new FormatException("Magic number doesn't match");
+            
+            var result = new VaultHeader();
+
+            var mpIV = binReader.ReadBytes(AesSizes.IV);
+            var mpAuthentication = binReader.ReadBytes(AesSizes.Auth);
+            var encryptedMp = binReader.ReadBytes(AesSizes.Key);
+                
+            result.MasterPassword = new MasterPassword(mpIV, mpAuthentication, encryptedMp);
+
+            return result;
+        }
+    }
+}
