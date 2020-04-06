187425a581e0cb83f05d170dc50dab0172694ed6
slimetsp <theslimeplay1@gmail.com>
2020-04-06 17:31:47 +0200

    Implemented extension for streams which copies the content and also creates a hash

--- crypto.Core/Cryptography/VerifyingStream.cs
+++ crypto.Core/Cryptography/VerifyingStream.cs
@@ -1,18 +1,46 @@
 using System;
+using System.Buffers;
 using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace crypto.Core.Cryptography
 {
     public static class VerifyingStream
     {
-        public static byte[] CopyToCreateHash(this Stream source, Stream destination)
+        // default size in c# docs about Stream.CopyTo(Stream, Int32)
+        public const int BufferSize = 81920;
+    
+        public static async Task<byte[]> CopyToCreateHashAsync(this Stream source, Stream destination)
         {
-            throw new NotImplementedException();
-        }
-        
-        public static bool CopyToVerifyHash(this Stream source, Stream destination, byte[] verificationHash)
-        {
-            throw new NotImplementedException();
+            using var sha = SHA256.Create();
+            var hash = new byte[32];
+
+            var buffer = ArrayPool<byte>.Shared.Rent(BufferSize);
+
+            try
+            {
+                int readBytes;
+                while((readBytes = await source.ReadAsync(buffer)) != 0)
+                {
+                    await destination.WriteAsync(buffer, 0, readBytes);
+                    
+                    if (readBytes >= BufferSize)
+                    {
+                        sha.TransformBlock(buffer, 0, buffer.Length, hash, 0);
+                    }
+                    else
+                    {
+                        sha.TransformFinalBlock(buffer, 0, readBytes);
+                    }
+                }
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+
+            return sha.Hash;
         }
     }
 }
