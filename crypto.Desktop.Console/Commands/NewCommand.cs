860587a1916ddb32746434a304ae6e7f4e7feb49
slimetsp <theslimeplay1@gmail.com>
2020-04-08 16:33:04 +0200

    Implemented new command

--- crypto.Desktop.Console/Commands/NewCommand.cs
+++ crypto.Desktop.Console/Commands/NewCommand.cs
@@ -1,4 +1,8 @@
-﻿using Serilog;
+﻿using System;
+using System.IO;
+using crypto.Core;
+using crypto.Core.Extension;
+using Serilog;
 
 namespace crypto.Desktop.Cnsl.Commands
 {
@@ -13,13 +17,26 @@ namespace crypto.Desktop.Cnsl.Commands
         // new name -> creates a new directory with that name and uses that
         // new name path -> goes to that path and creates a new directory with the given name and uses that
 
-        public string? Name { get; }
-        public string? Path { get; }
+        private string? Name { get; }
+        private string? Path { get; }
 
         public override void Run()
         {
             Log.Debug("Running NewProject with " +
                       $"Name = {Name ?? "null"}, Path = {Path ?? "null"}");
+
+            var key = PasswordPrompt.PromptPasswordWithConfirmation().ApplySHA256();
+
+            var vaultName = Name ?? GetCurrentDirectoryName();
+            using var vault = Vault.Create(vaultName, key, Path);
+            
+            Notifier.Success("Created vault " + vaultName);
+        }
+
+        private static string GetCurrentDirectoryName()
+        {
+            var currentDir = new DirectoryInfo(Environment.CurrentDirectory);
+            return currentDir.Name;
         }
     }
 }
