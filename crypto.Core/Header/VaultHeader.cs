d779c196161f13e599b8f90276b677c10d08d5c7
slimetsp <theslimeplay1@gmail.com>
2020-04-08 09:54:02 +0200

    Added version to magic number

--- crypto.Core/Header/VaultHeader.cs
+++ crypto.Core/Header/VaultHeader.cs
@@ -4,7 +4,7 @@ namespace crypto.Core.Header
 {
     public class VaultHeader
     {
-        public static readonly byte[] MagicNumber = {0x6e, 0x76, 0x66};
+        public static readonly byte[] MagicNumber = {0x6e, 0x76, 0x66, 0x01};
         public static readonly int MagicNumberLength = MagicNumber.Length;
 
         private VaultHeader(MasterPassword masterPassword)
