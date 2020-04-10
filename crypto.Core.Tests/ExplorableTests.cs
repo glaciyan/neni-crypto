cd634bf855694377adaf5ac5bcbfbac3c2095263
slimetsp <theslimeplay1@gmail.com>
2020-04-10 12:01:02 +0200

    Added simple string based file explorer

--- crypto.Core.Tests/ExplorableTests.cs
+++ crypto.Core.Tests/ExplorableTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using crypto.Core.FileExplorer;
+using NUnit.Framework;
 
 namespace crypto.Core.Tests
 {
@@ -11,7 +12,18 @@ namespace crypto.Core.Tests
             var path1 = "/something/other/pictures/stuff/picture1.png";
             var path2 = "/something/other/secret/encrypted/secretFile.crp";
 
-            var explorer = 
+            var pathToExplore = "/something/other/";
+            var pathToExplore2 = "/something/other/secret";
+            var pathToExplore3 = "/something/other/secret/encrypted/secretFile.crp";
+
+
+            var explorer = new Explorer(path1, path2);
+
+            var files1 = explorer.GetFromPath(pathToExplore);
+            var files2 = explorer.GetFromPath(pathToExplore2);
+            var files3 = explorer.GetFromPath(pathToExplore3);
+
+            
         }
     }
 }
--- crypto.Core/FileExplorer/Explorer.cs
+++ crypto.Core/FileExplorer/Explorer.cs
@@ -1,7 +1,63 @@
+using System;
+using System.Collections.Generic;
+
 namespace crypto.Core.FileExplorer
 {
     public class Explorer
     {
-        
+        public List<string[]> FilePaths { get; } = new List<string[]>();
+
+        public Explorer(params string[] files)
+        {
+            foreach (var path in files)
+            {
+                AddFile(path);
+            }
+        }
+
+        public void AddFile(string path)
+        {
+            FilePaths.Add(path.Replace('\\', '/').Split('/', StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public Dictionary<string[], FileFolder> GetFromPath(string position)
+        {
+            var split = position.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            var matchingFiles = new Dictionary<string[], FileFolder>();
+
+            foreach (var path in FilePaths)
+            {
+                var matches = true;
+                var i = 0;
+
+                for (;i < split.Length; i++)
+                {
+                    matches = matches && split[i] == path[i];
+                }
+
+                var fileFolder = i == path.Length ? FileFolder.File : FileFolder.Folder;
+                if (matches) matchingFiles.Add(path, fileFolder);
+            }
+
+            return matchingFiles;
+        }
+
+        private static IEnumerable<T> CopyList<T>(List<T> list)
+        {
+            var result = new List<T>(list.Capacity);
+
+            foreach (var item in list)
+            {
+                result.Add(item);
+            }
+
+            return result;
+        }
+    }
+
+    public enum FileFolder
+    {
+        File,
+        Folder
     }
 }
