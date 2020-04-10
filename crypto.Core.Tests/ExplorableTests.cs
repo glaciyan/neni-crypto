622a3c63f5c2e60f17ce20c2f39f87fd4c954d6a
slimetsp <theslimeplay1@gmail.com>
2020-04-10 10:58:44 +0200

    Renamed Explorable

--- crypto.Core.Tests/ExplorableTests.cs
+++ crypto.Core.Tests/ExplorableTests.cs
@@ -5,6 +5,13 @@ namespace crypto.Core.Tests
     [TestFixture]
     public class ExplorableTests
     {
-        
+        [Test]
+        public void GettingCorrectCollection()
+        {
+            var path1 = "/something/other/pictures/stuff/picture1.png";
+            var path2 = "/something/other/secret/encrypted/secretFile.crp";
+
+            var explorer = 
+        }
     }
 }
